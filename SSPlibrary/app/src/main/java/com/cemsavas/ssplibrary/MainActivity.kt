4a1eb939fc407c7b21d12da6e70b6a4e01e55ae8
Cemsavas <cemsavas1986@hotmail.com>
2022-08-23 16:34:16 +0300

    SPP Lİbrary
    SSP (Size Scalable Pixels) is easy library to adjust textsizes for all screensizes

---
+++ SSPlibrary/app/src/main/java/com/cemsavas/ssplibrary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cemsavas.ssplibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
